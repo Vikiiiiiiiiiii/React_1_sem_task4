4815e22ee1be901ec1d2879b3ef3929d49369193
vika <viktoriaphilphak@mail.ru>
2025-02-26 00:43:01 +0300

    seminar task4 dop

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,5 +1,5 @@
 function EventCard(props){
-    const now = new Date().toLocaleTimeString();
+    
     return(
         <div>
             <h1 className = "heading">Событие: {props.name} состоится: {props.data} в {props.place}.</h1>
